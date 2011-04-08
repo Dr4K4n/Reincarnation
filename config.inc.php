a28bb8b2e950fe504ea3577fe127e7e5951e8c9a
Stefan <stefan@dr4k4n.de>
2011-04-08 14:46:43 +0200

    changed password

--- config.inc.php
+++ config.inc.php
@@ -3,6 +3,6 @@ error_reporting(E_ERROR);
 
 $_SQL['host'] = 'localhost';
 $_SQL['user'] = 'reincarnation';
-$_SQL['pass'] = 'youngcoder';
+$_SQL['pass'] = 'HCBjBW5XRUEEfA8w';
 $_SQL['db'] = 'reincarnation';
 ?>
